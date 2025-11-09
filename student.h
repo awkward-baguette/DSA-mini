c84bb44fc77b3310b3ffacbbc27947d9f012ffe2
Roopa Sreedhar A <193498568+coderRoopa@users.noreply.github.com>
2025-11-09 17:36:20 +0530

    Add student data structures and function prototypes

---
+++ student.h
@@ -0,0 +1,62 @@
+// student.h
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SUBJECTS 6
+#define MAXHISTORY 10
+
+typedef struct subject {
+    int credit;
+    char name;
+    int marks;
+    char grade;
+} subnode;
+
+typedef struct sem {
+    int semno;
+    subnode sublist[SUBJECTS];
+    float sgpa;
+    struct sem *next;
+} semnode;
+
+typedef struct gradehistory {
+    int semno;
+    char subname[20];
+    int oldmarks;
+    int newmarks;
+    char oldgrade;
+    char newgrade;
+} historynode;
+
+typedef struct snode {
+    int roll_no;
+    char name[50];
+    semnode *semhead;
+    historynode history[MAXHISTORY];
+    int historylength;
+    struct snode *left;
+    struct snode *right;
+} studentnode;
+
+typedef struct tree {
+    studentnode *root;
+} tree;
+
+
+void init(tree *t);
+studentnode *create_snode(int r_no, char *sname);
+void add_new_student(tree *t, char *sname, int roll_no);
+void addsem(tree *t, int semno, subnode *subjects);
+float find_sgpa(subnode subjects[]);
+float compute_cgpa(semnode *semhead);
+char getgrade(int marks);
+int getWeightage(char ch);
+void addmarks(tree *t, char *sname, int roll_no, int semno, char *subname, float marks);
+
+
+#endif
+
