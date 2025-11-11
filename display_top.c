6b13afbd37adeac2b10940fcb2b8cd4757510de5
ridhima-jain45 <222589127+ridhima-jain45@users.noreply.github.com>
2025-11-11 20:30:32 +0530

    Update display_top.c

--- display_top.c
+++ display_top.c
@@ -6,6 +6,7 @@ struct node
 {
 	int key;
 	float pty_cgpa;
+    studentnode *student;
 	struct node *link;
 };
 typedef struct node node_t;
@@ -17,15 +18,16 @@ struct list
 typedef struct list list_t;
 
 void pqinsert(list_t*p,studentnode*s){
-    int r_no=s->roll_no;
+
     float cgpa=compute_cgpa(s->semhead);
     node_t *temp,*pres,*prev;
     pres=p->head;
     prev=NULL;
     temp=(node_t*)malloc(sizeof(node_t));
-		temp->key = r_no;
+		temp->key = s->roll_no;
         temp->pty_cgpa=cgpa;
         temp->link=NULL;
+        temp->student=s;
     if(p->head==NULL){
         p->head=temp;
     }
@@ -48,30 +50,27 @@ void pqinsert(list_t*p,studentnode*s){
     }
 }
 
-node_t pqdelete(list_t *ptr_list)
-{
-	node_t temp, *pres;
-	if(ptr_list->head==NULL)
-	{
-		printf("Empty queue..Cannot delete");
-		temp.key=-1;
-		temp.pty_cgpa=-1;
-	}
-	else
-	{
-	  pres=ptr_list->head;
-	  temp.key= pres->key;
-	  temp.pty_cgpa=pres->pty_cgpa;
-	  if(pres->link==NULL) 
-		  ptr_list->head =NULL;
-	  else
-		   ptr_list->head=pres->link; 
-	   free(pres);
-       return temp;
-	}
-}	
+node_t pqdelete(list_t *ptr_list) {
+    node_t temp;
+    temp.key = -1;
+    temp.pty_cgpa = -1;
 
-void qdisplay(list_t *ptr_list, studentnode*s)
+    if (ptr_list->head == NULL) {
+        printf("Empty queue..Cannot delete\n");
+        return temp;
+    }
+
+    node_t *pres = ptr_list->head;
+    temp.key = pres->key;
+    temp.pty_cgpa = pres->pty_cgpa;
+
+    ptr_list->head = pres->link;
+    free(pres);
+
+    return temp;
+}
+
+void qdisplay(list_t *ptr_list)
  {
 	    node_t *pres;
 	    pres = ptr_list->head;
@@ -83,8 +82,8 @@ void qdisplay(list_t *ptr_list, studentnode*s)
     	   printf("\n");
 	      while(pres!=NULL)
 	     {
-		    printf("name= %s->",s->name);
-			printf("cgpa = %d->",pres->pty_cgpa);
+		    printf("name= %s->",pres->student->name);
+			printf("cgpa = %.2f->",pres->pty_cgpa);
 			printf("\n");
 		    pres=pres->link;
 	      }
