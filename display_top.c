48d0ff7c4d4a301927b4cc0f9d1adebae2c7253b
ridhima-jain45 <222589127+ridhima-jain45@users.noreply.github.com>
2025-11-18 10:40:27 +0530

    Update display_top.c

--- display_top.c
+++ display_top.c
@@ -50,28 +50,41 @@ void pqinsert(list_t*p,studentnode*s){
     }
 }
 
-node_t pqdelete(list_t *ptr_list) {
+node_t pqdelete(list_t *ptr_list, int r_no) {
     node_t temp;
     temp.key = -1;
     temp.pty_cgpa = -1;
+    temp.student = NULL;
 
-    if (ptr_list->head == NULL) {
-        printf("Empty queue..Cannot delete\n");
+    if (ptr_list->head == NULL)
         return temp;
-    }
 
     node_t *pres = ptr_list->head;
+    node_t *prev = NULL;
+
+    while (pres != NULL && pres->key != r_no) {
+        prev = pres;
+        pres = pres->link;
+    }
+
+    if (pres == NULL)
+        return temp;
+
     temp.key = pres->key;
     temp.pty_cgpa = pres->pty_cgpa;
+    temp.student = pres->student;
 
-    ptr_list->head = pres->link;
+    if (prev == NULL)
+        ptr_list->head = pres->link;
+    else
+        prev->link = pres->link;
+
     free(pres);
-
     return temp;
 }
 
-void qdisplay(list_t *ptr_list)
- {
+void qdisplay(list_t *ptr_list,int n)
+ {     int count=0;
 	    node_t *pres;
 	    pres = ptr_list->head;
 
@@ -80,11 +93,12 @@ void qdisplay(list_t *ptr_list)
 	   else
  	     {		
     	   printf("\n");
-	      while(pres!=NULL)
+	      while(pres!=NULL &&count<n)
 	     {
 		    printf("name= %s->",pres->student->name);
-			printf("cgpa = %.2f->",pres->pty_cgpa);
+			printf("cgpa = %f->",pres->pty_cgpa);
 			printf("\n");
+			count++;
 		    pres=pres->link;
 	      }
 	}
